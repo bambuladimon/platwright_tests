e6cfce02803778e367fb693aa1668abaac1e36bc
Dmytro Kulikovskyi <dmytro.kulikovskyi@intellias.com>
2024-11-24 11:55:56 +0200

    Delete login infcredentials from source code

--- test_login.py
+++ test_login.py
@@ -3,8 +3,8 @@ from playwright.sync_api import Page, expect
 
 def test_login_correct_data(page:Page):
     page.goto('https://app.testomat.io/')
-    page.locator('#content-desktop #user_email').fill('dmytro.kulikovskyi@intellias.com')
-    page.locator('#content-desktop #user_password').fill('13579_Dimon4IK')
+    page.locator('#content-desktop #user_email').fill('---insert your email---')
+    page.locator('#content-desktop #user_password').fill('---insert your password---')
     page.get_by_role('button', name='Sign In').click()
     expect(page.get_by_text('Signed in successfully')).to_be_visible()
 
